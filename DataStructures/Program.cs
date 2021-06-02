f0cd4951ae5c3483dbc99a722772b4caefb916d1
Sameer <sameer.b.ghorpade.-nd@disney.com>
2021-06-02 22:43:31 +0530

    Dynamic Arrays

---
+++ DataStructures/Program.cs
@@ -0,0 +1,16 @@
+﻿using Data.Structure.Arrays;
+using System;
+
+namespace Data.Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDynamicArray ds = new DynamicArray();
+            ds.Insert(10);
+            ds.Print();
+
+        }
+    }
+}
